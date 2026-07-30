23087f6bfd29f70e2a87a7d6032f15b9a84b014c
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-07-30 12:18:02 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
